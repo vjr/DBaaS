19db412268ea93d7ae0ab1f48e0932bc7d9516bf
Vishal Rao <vishalrao@gmail.com>
2022-07-21 12:38:41 +0530

    Get http response code properly

--- zeppelin/zeppelin-test.sh
+++ zeppelin/zeppelin-test.sh
@@ -12,15 +12,15 @@ echo "VJR: Done waiting for subshell of: Starting zeppelin..."
 
 echo "VJR: Sleeping..."
 
-sleep 5
+sleep 2
 
 echo "VJR: Done sleeping."
 
 run_paragraph()
 {
   echo "VJR: Start run paragraph $2 for notebook $1"
-  curl -o /dev/null -s -w {%http_code} -X POST http://localhost:8081/api/notebook/run/$1/$2
-  echo "VJR: End run paragraph $2 for notebook $1, http_code=$http_code"
+  zep_code=$(curl -o /dev/null -s -w %{http_code} -X POST http://localhost:8081/api/notebook/run/$1/$2)
+  echo "VJR: End run paragraph $2 for notebook $1, zep_code=$zep_code, http_code=$http_code"
 }
 
 result_code=0
@@ -31,12 +31,13 @@ try_run_paragraph()
 (
 retry_count=5
 sleep_time=1
+zep_code=0
 
 echo "VJR: Try run paragraph $2 for notebook $1"
 run_paragraph $1 $2
 echo "VJR: Done try run paragraph $2 for notebook $1"
 
-while [ "$http_code" != "200" ] && [ $retry_count -gt 0 ];
+while [ "$zep_code" != "200" ] && [ $retry_count -gt 0 ];
 do
   ((retry_count--))
   echo "VJR: Sleep $sleep_time seconds before retry para $2 note $1"
@@ -48,7 +49,7 @@ do
   echo "VJR: After retry para $2 note $1"
 done
 
-if [ $retry_count -eq 0 ] && [ "$http_code" != "200" ]; then
+if [ $retry_count -eq 0 ] && [ "$zep_code" != "200" ]; then
   echo "VJR: Setting error code for para $2 note $1"
   result_code=1
 fi
@@ -58,7 +59,7 @@ fi
 
 echo "VJR: Running paras..."
 
-try_run_paragraph note1 para1
+try_run_paragraph 2EYUV26VR 20180530-101118_380906698
 
 echo "VJR: Before final wait"
 
