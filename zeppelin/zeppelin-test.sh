e138365f9223568e25152f395deaaa330e814c78
Vishal Rao <vishalrao@gmail.com>
2022-07-22 07:14:28 +0530

    Rework exit code handling

--- zeppelin/zeppelin-test.sh
+++ zeppelin/zeppelin-test.sh
@@ -3,16 +3,17 @@
 echo "VJR: Starting zeppelin from: [$ZEPPELIN_HOME]"
 
 ("$ZEPPELIN_HOME"/bin/zeppelin-daemon.sh start) &
+zeppelinPID="$!"
 
 echo "VJR: Waiting for subshell of: Starting zeppelin..."
 
-wait
+#wait
 
 echo "VJR: Done waiting for subshell of: Starting zeppelin..."
 
 echo "VJR: Sleeping..."
 
-sleep 5
+sleep 1
 
 echo "VJR: Done sleeping."
 
@@ -23,8 +24,6 @@ run_paragraph()
   echo "VJR: End run paragraph $2 for notebook $1, zep_code=$zep_code, http_code=$http_code"
 }
 
-result_code=0
-
 try_run_paragraph()
 {
 
@@ -32,45 +31,70 @@ try_run_paragraph()
 retry_count=5
 sleep_time=1
 zep_code=0
-result_code=0
 
-echo "VJR: Try run paragraph $2 for notebook $1"
+#echo "VJR: Try run paragraph $2 for notebook $1"
 run_paragraph $1 $2
-echo "VJR: Done try run paragraph $2 for notebook $1"
+#echo "VJR: Done try run paragraph $2 for notebook $1"
 
 while [ "$zep_code" != "200" ] && [ $retry_count -gt 0 ];
 do
   ((retry_count--))
-  echo "VJR: Sleep $sleep_time seconds before retry para $2 note $1"
+  #echo "VJR: Sleep $sleep_time seconds before retry para $2 note $1"
   sleep $sleep_time
-  echo "VJR: Done sleep $sleep_time seconds before retry para $2 note $1"
+  #echo "VJR: Done sleep $sleep_time seconds before retry para $2 note $1"
   ((sleep_time*=2))
-  echo "VJR: Before retry para $2 note $1"
+  #echo "VJR: Before retry para $2 note $1"
   run_paragraph $1 $2
-  echo "VJR: After retry para $2 note $1"
+  #echo "VJR: After retry para $2 note $1"
 done
 
 if [ $retry_count -eq 0 ] && [ "$zep_code" != "200" ]; then
-  echo "VJR: Setting error code for para $2 note $1"
-  result_code=1
+  #echo "VJR: Setting error code for para $2 note $1"
+  exit 1
+  #resultCodes+="1"
+#else
+#  echo 0
+  #resultCodes+="0"
 fi
-) &
-
+)
 }
 
 echo "VJR: Running paras..."
 
-try_run_paragraph 2EYUV26VR 20180530-101118_380906698
+declare -a childPIDs
+
+#(resultCodes+=$(try_run_paragraph 2EYUV26VR 20180530-101118_380906698)) &
+(try_run_paragraph 2EYUV26VR 20180530-101118_380906698) &
+childPIDs+=($!)
+
+(try_run_paragraph 2EYUV26VR 20180530-101118_380906698) &
+childPIDs+=($!)
 
 echo "VJR: Before final wait"
 
-wait
+wait "$zeppelinPID"
 
-echo "VJR: After final wait, result_code=$result_code"
+#wait
 
-if [ $result_code -ne 0 ]; then
-  echo "VJR: PHAIL!"
-  exit 1
-fi
+echo "VJR: After final wait, result count=${!childPIDs[@]}"
+
+for i in "${!childPIDs[@]}"; do
+  echo "VJR: Child PID $i: ${childPIDs[i]}"
+  if wait "${childPIDs[i]}"; then
+    echo "VJR: Successfully run paragraph $i"
+  else
+    echo "VJR: PHAIL!!!"
+    #exit $?
+    echo $?
+  fi
+done
+
+#for i in "${!resultCodes[@]}"; do
+#  resultCode="${resultCodes[i]}"
+#  if [ "$resultCode" = "1" ]; then
+#    echo "VJR: PHAIL!"
+#    exit 1
+#  fi
+#done
 
 echo "VJR: SUCCESS!"
